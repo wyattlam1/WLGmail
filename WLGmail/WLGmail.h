880aa56ae1fb8e244be625d767d6ff83bef03833
Wyatt Lam <wyatt.lam1@gmail.com>
2014-09-06 16:50:30 -0700

    created initial project file

---
+++ WLGmail/WLGmail.h
@@ -0,0 +1,13 @@
+//
+//  WLGmail.h
+//  WLGmail
+//
+//  Created by Wyatt Lam on 9/6/14.
+//  Copyright (c) 2014 WyattLam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WLGmail : NSObject
+
+@end
---
+++ WLGmail/WLGmail.m
@@ -0,0 +1,13 @@
+//
+//  WLGmail.m
+//  WLGmail
+//
+//  Created by Wyatt Lam on 9/6/14.
+//  Copyright (c) 2014 WyattLam. All rights reserved.
+//
+
+#import "WLGmail.h"
+
+@implementation WLGmail
+
+@end
